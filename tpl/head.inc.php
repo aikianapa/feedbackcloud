32c0e16c4ce225fb17c043fe0ccd5875e698fb9d
Oleg Frolov <oleg_frolov@mail.ru>
2020-07-06 10:47:04 +0300

    update

--- tpl/head.inc.php
+++ tpl/head.inc.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" type="image/png" sizes="16x16" href="assets/img/favicon.png">
     <link rel="alternate" href="http://feedbackcloud.ru/" hreflang="x-default">
     <title>FeedbackCloud - сервис обратной связи</title>
-    <link rel="stylesheet" href="assets/css/loader.css">
+    <link rel="stylesheet" href="//feedbackcloud.ru/tpl/assets/css/loader.css">
     <wb-snippet name="wbapp" />
     <wb-snippet name="bootstrap" />
     <wb-snippet name="fontawesome4" />
@@ -20,20 +20,20 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Feedbackcloud">
     <meta name="twitter:description" content="Feedbackcloud - Получайте обратную связь от клиентов online">
-    <meta name="twitter:image" content="assets/img/icon-384x384.png">
+    <meta name="twitter:image" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
 
     <!-- Facebook -->
     <meta property="og:url" content="https://feedbackcloud.ru/">
     <meta property="og:title" content="Feedbackcloud">
     <meta property="og:description" content="Feedbackcloud - Получайте обратную связь от клиентов online">
-    <meta property="og:image" content="assets/img/icon-384x384.png">
-    <meta property="og:image:secure_url" content="assets/img/icon-384x384.png">
+    <meta property="og:image" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+    <meta property="og:image:secure_url" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
     <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="300">
     <meta property="og:image:height" content="300">
 
 
-    <link name="image_src" href="assets/img/icon-384x384.png">
+    <link name="image_src" href="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
     <link rel="shortcut icon" href="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
     <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-72x72.png" sizes="76x76">
     <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-96x96.png" sizes="120x120">
