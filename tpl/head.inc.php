e981dae7072b07304effb779e3edd4e052563163
Oleg Frolov <oleg_frolov@mail.ru>
2020-07-16 21:30:26 +0300

    modified:   tpl/head.inc.php

--- tpl/head.inc.php
+++ tpl/head.inc.php
@@ -1,7 +1,45 @@
 <head>
     <meta charset="UTF-8">
-    <!-- Required meta tags -->
+    <wb-var base="/tpl" />
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+<!--    <link rel="shortcut icon" href="/tpl/assets/img/favicon.png">-->
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="assets/img/favicon.png">
+    <link rel="alternate" href="http://feedbackcloud.ru/" hreflang="x-default">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="https://feedbackcloud.ru/">
+    <meta name="twitter:creator" content="feedbackcloud.ru">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Feedbackcloud">
+    <meta name="twitter:description" content="Feedbackcloud - Получайте обратную связь от клиентов online">
+    <meta name="twitter:image" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="https://feedbackcloud.ru/">
+    <meta property="og:title" content="Feedbackcloud">
+    <meta property="og:description" content="Feedbackcloud - Получайте обратную связь от клиентов online">
+    <meta property="og:image" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+    <meta property="og:image:secure_url" content="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
+
+
+    <link name="image_src" href="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+    <link rel="shortcut icon" href="//feedbackcloud.ru/tpl/assets/img/icon-384x384.png">
+    <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-72x72.png" sizes="76x76">
+    <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-96x96.png" sizes="120x120">
+    <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-144x144.png" sizes="152x152">
+    <link rel="apple-touch-icon" href="//feedbackcloud.ru/tpl/assets/img/icon-152x152.png" sizes="180x180">
+
+    <!-- Meta -->
+    <meta name="description" content="Feedbackcloud - Получайте обратную связь от клиентов online">
+    <meta name="author" content="Feedbackcloud">
+    <meta property="og:site_name" content="Feedbackcloud">
+    
     <title>{{header}}</title>
     <wb-snippet name="wbapp" />
     <wb-snippet name="bootstrap" />
